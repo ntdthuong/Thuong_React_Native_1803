744a38cc63217ccf9a48cfdeeb08a334a0118d6f
thuong.ntd <thuong.ntd@sutrixsolutions.com>
2018-03-19 17:49:20 +0700

    edit variable

--- bt3.js
+++ bt3.js
@@ -1,10 +1,10 @@
-function bt3() {
+function bt3(dong) {
   let tr = document.querySelectorAll('tr');
-  for (var i=0; i < tr.length; i++) {
+  for (var i=0; i < dong; i++) {
     for (var j=0; j <= i; j++) {
       let td = tr[i].children;
       td[j].innerHTML = '*';
     }
   }
 }
-bt3();
+bt3(8);
