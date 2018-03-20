2ed13a7c0234e078e31ca1e5c010630fe1e3c624
thuong.ntd <thuong.ntd@sutrixsolutions.com>
2018-03-20 11:16:22 +0700

    bt4

--- bt3.js
+++ bt3.js
@@ -8,3 +8,11 @@ function bt3(dong) {
   }
 }
 bt3(8);
+let dong = 8;
+for (let i = 1; i <= dong; i++) {
+  let row = '';
+  for (let j = 1; j < i; j++) {
+    row += j;
+  }
+  document.write(row + i + '<br/>');
+}
